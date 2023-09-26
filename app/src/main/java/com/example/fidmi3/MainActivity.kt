1601f62ea16fc0d2fe24cb1dcd1e49d3d643ea02
schaked <schaked@post.bgu.ac.il>
2023-09-26 14:35:14 +0300

    Fixed menu

--- app/src/main/java/com/example/fidmi3/MainActivity.kt
+++ app/src/main/java/com/example/fidmi3/MainActivity.kt
@@ -7,15 +7,18 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var statusText: TextView
+    private lateinit var statusText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        statusText = findViewById(R.id.statusText)
+        statusText = findViewById(R.id.statusText)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater
@@ -28,5 +31,12 @@ class MainActivity : AppCompatActivity() {
             R.id.ingredients -> Toast.makeText(this, "ingredientssss", Toast.LENGTH_SHORT).show()
         }
         return super.onOptionsItemSelected(item)
+//        return when (item.itemId){
+//            R.id.ingredients -> {
+//                statusText.text = "ingredientssss"
+//                true
+//            }
+//            else -> super.onOptionsItemSelected(item)
+//        }
     }
 }
